362c60595aca48adf693fc817d5bc5cc87500d3b
Darya_Savitskaya <statusqvogerz@gmail.com>
2022-09-12 21:34:33 +0500

    L3_T2

---
+++ index.js
@@ -0,0 +1,11 @@
+let myNumber = 300
+let myString = 'This is single quotes string'
+let myBoolean = true
+
+myNumber = 'This is a string also'
+myString = 12345
+myBoolean = null
+
+alert(myNumber)
+alert(myString)
+alert(myBoolean)
